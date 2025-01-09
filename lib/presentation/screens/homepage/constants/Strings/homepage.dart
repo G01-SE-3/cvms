2950276a346cc8f9e8c434356c72cc4a3f6f8307
unknown <safia.tifour@ensia.edu.dz>
2025-01-09 15:39:57 +0100

    homepage_add_buttons+del_appbar_IO

--- lib/presentation/screens/homepage/constants/Strings/homepage.dart
+++ lib/presentation/screens/homepage/constants/Strings/homepage.dart
@@ -4,4 +4,9 @@ class HomePageStrings {
   static const String inspectors = 'Inspectors';
   static const String errorMessage = 'Error: ';
   static const String monthlyPVsEvolution = 'Monthly PVs Evolution';
+
+  static const String addPV = 'Add PV';
+  static const String addBusinessOffender = 'Add Business Offender';
+  static const String addIndividualOffender = 'Add Individual Offender';
+  static const String addInspector = 'Add Inspector';
 }
